58ba70dc13ef31fcb9b88dbcbcf39b0f5eaffacd
Mak-837 <resemkasa@yandex.ru>
2024-03-06 18:14:57 +0300

    thirdCommit

--- fapi.py
+++ fapi.py
@@ -1,16 +1,19 @@
 from fastapi import FastAPI
-from datetime import datetime
 import os
+from datetime import datetime
+import uvicorn
 app = FastAPI()
 
 
 @app.get("/time")
-def date_time():
+def time():
     return {"current_time": datetime.now()}
 
 
 @app.get("/path")
 def path():
-    return os.path.abspath('fapi.py')
+    return os.path.abspath("fapi.py")
 
-#first
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
